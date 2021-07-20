c5318e399146e92fdfd4b7c5a393a0ee84ecfcc6
Marko Benacic <benacicmarko@gmail.com>
2021-07-21 01:18:14 +0200

    made changes requested in code review

--- TV shows/TV shows/Login/LoginViewController.swift
+++ TV shows/TV shows/Login/LoginViewController.swift
@@ -8,24 +8,28 @@
 import Foundation
 import UIKit
 
-
-
-
 class LoginViewController : UIViewController{
+    
+    //MARK: - Outlets
+    
+    @IBOutlet private weak var numOfTapsLabel: UILabel!
+    
+    //MARK: - Properties
     
     var numberOfTaps = 0
     
-    @IBOutlet weak var label: UILabel!
-    
-    @IBAction func buttonPrint(_ sender: Any) {
-        numberOfTaps += 1
-        print(numberOfTaps)
-        label.text = "taps: " + String(numberOfTaps)
-    }
+    //MARK: - Lifecycle methods
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
     }
+    
+    //MARK: - Actions
+    
+    @IBAction private func tapButtonActionHandler(_ sender: Any) {
+        numberOfTaps += 1
+        print(numberOfTaps)
+        numOfTapsLabel.text = "taps: " + String(numberOfTaps)
+    }
+    
 }
